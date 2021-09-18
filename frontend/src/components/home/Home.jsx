edc3703ab8a432d9b22370ebabb91685c210c4f8
Gabriel Alves Soares <gabriel.alves.gas@hotmail.com>
2021-09-18 00:45:28 -0300

    START

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo Projeto do capitulo de React.">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema desenvolvido para exemplificar um cadastro em React</p>
+    </Main>
